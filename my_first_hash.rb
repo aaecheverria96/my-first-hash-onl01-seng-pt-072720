57e92768bfd30ca9e45e7e22d6ffc7c5f91ac88f
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-08 04:43:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,5 @@
 def my_hash
-  my_hash = {
-  "Song Title"=> "Muchacha"
-  "Playlist" => "Para Bailar"
-  }# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"my_name" => "Andrew Echeverria"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
 end
